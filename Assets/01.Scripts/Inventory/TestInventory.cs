a65b7818d48979546924838d2141c7767b5a8a60
boguen <qhrms012@naver.com>
2025-06-11 18:52:26 +0900

    [add] 테스트아이템 추가 인벤토리 구현

--- Assets/01.Scripts/Inventory/TestInventory.cs
+++ Assets/01.Scripts/Inventory/TestInventory.cs
@@ -6,12 +6,18 @@ public class TestInventory : MonoBehaviour
 {
     public Inventory inventory;
     public ItemData testItem;
+    public ItemData testItem2;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
             inventory.AddItem(testItem);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            inventory.AddItem(testItem2);
         }
     }
 
